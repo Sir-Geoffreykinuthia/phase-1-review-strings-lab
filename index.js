ac166056362608117a7a63beb973dd8205f847be
geoffrey kinuthia <geoffreykinuthia10@gmail.com>
2023-03-20 07:40:28 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-// Write your code in this file!
+const currentUser = "Shabby Shawn Lashon";
+const welcomeMessage = 'Welcome to Flatbook, ' + currentUser + '!';
+const WelcomeMessage = `Welcome to Flatbook, ${currentUser}`;
+const excitedWelcomeMessage = WelcomeMessage.toUpperCase() +'!';
+const shortGreeting = `Welcome, ${currentUser.slice(0,1)}!`;
